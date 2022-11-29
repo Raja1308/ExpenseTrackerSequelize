fb21baf1cb4cb808e8323ffd5606596fac69d1a7
Raja <rjjaital@gmail.com>
2022-11-29 20:04:46 +0530

    expense screen

--- backend/route/expense.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const route = express.Router()
-const expenseController = require('../Controller/expense')
-const authorization = require('../authorization/authorization')
-
-route.post('/home', authorization.authenticateToken,expenseController.postExpense)
-//route.post('/signin', userController.postLogin)
-
-module.exports=route
